84a86f0c27d30222220ccbff2140052340370319
Asmaa Elkholy <asmaaelkholy6@gmail.com>
2025-01-28 13:44:49 +0300

    Initial commit of MyLibrary pod

---
+++ Example/Pods/Target Support Files/MiniAppLibrary/MiniAppLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MiniAppLibrary : NSObject
+@end
+@implementation PodsDummy_MiniAppLibrary
+@end
---
+++ Example/Pods/Target Support Files/Pods-MiniAppLibrary_Tests/Pods-MiniAppLibrary_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MiniAppLibrary_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MiniAppLibrary_Tests
+@end
